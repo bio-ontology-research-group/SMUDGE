0d8fb5e51e0f0aee2459f4585f47c89e5359cec4
monaalsh <mona.alshahrani99@gmail.com>
2017-11-07 13:29:04 +0300

    remove gene intermediate nodes from walk

--- smudge_phenos.py
+++ smudge_phenos.py
@@ -25,7 +25,10 @@ def smudge_phenos(graph, num_walk, genes_without_phenos, super_classes, genes_ph
     genes_with_phenos = genes_phenos.keys()
     genes_nodes = [node for node in nodes if node.isdigit()]
     disease_nodes = [node for node in nodes if node.startswith('OMIM:')]
-    
+    print('Number of genes nodes: {}'.format(len(genes_nodes)))
+    print('Number of disease nodes: {}'.format(len(disease_nodes)))
+
+
     for dis in disease_nodes: #walk over disease nodes
       for walk in range(num_walk):
           path = []
@@ -42,28 +45,20 @@ def smudge_phenos(graph, num_walk, genes_without_phenos, super_classes, genes_ph
     for node in genes_nodes: #walk over PPI nodes
         for walk in range(num_walk):
           path = []
-          if node in genes_phenos:
-              phenos = genes_phenos[node]
-              pheno = random.choice(phenos)
-              if pheno in super_classes:
-                  path2root = super_classes[pheno]
-                  path.append(node) #gene with pheno
-                  path.append(pheno) #pheno
-                  path.extend(path2root)
-                  all_walks.append(path)
-              
-          elif node in genes_without_phenos: 
-              neigbors = graph.neighbors(node) 
-              rand_node = random.choice(neigbors)
+          neigbors = graph.neighbors(node)
+          ppi_adj = list(set(neigbors).intersection(genes_nodes))
+          if ppi_adj:
+              rand_node = random.choice(ppi_adj)
               stumbles = 0
+
               while(rand_node not in genes_with_phenos): #keep walking until find gene/w pheno
-                  if (stumbles > 5): #avoids stumbling blindly in the ppi
+                  if (stumbles > 5):
                       break
 
                   neigbors = graph.neighbors(rand_node) 
-                  rand_node = random.choice(neigbors)
-                  path.append(rand_node)
-                  stumbles = stumbles + 1
+                  if neigbors:
+                      rand_node = random.choice(neigbors)
+                      stumbles = stumbles + 1
 
               if stumbles > 5:
                   continue  
@@ -72,6 +67,7 @@ def smudge_phenos(graph, num_walk, genes_without_phenos, super_classes, genes_ph
               pheno = random.choice(phenos)
               if pheno in super_classes:
                 path2root = super_classes[pheno]
+                path.append(node)
                 path.append(pheno) #pheno
                 path.extend(path2root)
                 all_walks.append(path)
@@ -85,23 +81,23 @@ if __name__ == '__main__':
 
     data = '../../../Documents/smudge_data/'
 
-    graph = nx.read_edgelist(data+'mouse_graph.txt', create_using=nx.DiGraph(), data=(('label', str),))
-    genes_phenos = read_into_dict(data+'human_genes_mouse_phen.txt')
+    graph = nx.read_edgelist(data+'human_graph.txt', create_using=nx.DiGraph(), data=(('label', str),))
+    genes_phenos = read_into_dict(data+'human_genes_hpos.txt')
     disease_phenos = read_into_dict(data+'omim_hpos.txt')
-    super_classes = read_into_dict(data+'phenomNet_super_classes.txt')
-    genes_with_phenos = list(open(data+'genes_with_mouse_annotations.txt').readlines())
+    super_classes = read_into_dict(data+'hp_super_classes.txt')
+    genes_with_phenos = list(open(data+'genes_with_human_phenos.txt').readlines())
     genes_with_phenos = [item.strip() for item in genes_with_phenos]
-    genes_without_phenos = list(open(data+'genes_without_mouse_phenos.txt').readlines())
+    genes_without_phenos = list(open(data+'genes_without_human_phenos.txt').readlines())
     genes_without_phenos = [item.strip() for item in genes_without_phenos]
 
     print('The number of nodes in graph is: {}'.format(len(graph.nodes())))
     print('Walking PPIs and Phenotypes ...')
 
-    walks = smudge_phenos(graph, 500,genes_without_phenos, super_classes, genes_phenos, disease_phenos)
+    walks = smudge_phenos(graph, 500, genes_without_phenos, super_classes, genes_phenos, disease_phenos)
     print('Training the graph corpus...')
     model = Word2Vec(walks,size=512, window=10, min_count=1, sg =1, workers=24)
 
-    model.save_word2vec_format(data+'mouse_embeddings_500_1.txt')
+    model.save_word2vec_format(data+'human_embeddings_500_1.txt')
 
     pdb.set_trace()
 
